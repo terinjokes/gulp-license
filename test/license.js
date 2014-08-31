9d434a074abeb53af03601bcd4a380e2005a2aa3
Terin Stock <terinjokes@gmail.com>
2014-08-30 23:00:14 -0700

    Add code coverage support.

--- test/license.js
+++ test/license.js
@@ -6,14 +6,21 @@ var test = require('tape'),
 var testContentsInput = 'module.exports = void 0';
 var testContentsExpected = '/*! (c) 2014 Terin Stock (MIT) */\nmodule.exports = void 0';
 
-var testFile = new Vinyl({
+var testFile1 = new Vinyl({
 	cwd: '/home/terin/broken-promises/',
 	base: '/home/terin/broken-promises/test',
 	path: '/home/terin/broken-promises/test/test1.js',
 	contents: new Buffer(testContentsInput)
 });
 
-test('should add license to file', function(t) {
+var testFile2 = new Vinyl({
+	cwd: '/home/terin/broken-promises/',
+	base: '/home/terin/broken-promises/test',
+	path: '/home/terin/broken-promises/test/test2.js',
+	contents: new Buffer(testContentsInput)
+});
+
+test('should add tiny license to file', function(t) {
 	t.plan(7);
 
 	var stream = gulpLicense('MIT', {tiny: true, organization: 'Terin Stock'});
@@ -30,5 +37,30 @@ test('should add license to file', function(t) {
 		t.equals(String(newFile.contents), testContentsExpected);
 	});
 
-	stream.write(testFile);
+	stream.write(testFile1);
+});
+
+test('should add license to file', function(t) {
+	t.plan(11);
+
+	var stream = gulpLicense('MIT', {organization: 'Terin Stock'});
+
+	stream.on('data', function(newFile) {
+		var contents = String(newFile.contents);
+		t.ok(newFile, 'emits a file');
+		t.ok(newFile.path, 'file has a path');
+		t.ok(newFile.relative, 'file has relative path information');
+		t.ok(newFile.contents, 'file has contents');
+
+		t.ok(newFile instanceof Vinyl, 'file is Vinyl');
+		t.ok(newFile.contents instanceof Buffer, 'file contents are a buffer');
+
+		t.true(/The above copyright notice/.test(contents), 'contains MIT license');
+		t.false(/{{ year }}/.test(contents), 'does not contain template');
+		t.true(/2014/.test(contents), 'does contain the current year');
+		t.false(/{{ organization }}/.test(contents), 'does not contain template');
+		t.true(/Terin Stock/.test(contents), 'does contain the author name');
+	});
+
+	stream.write(testFile2);
 });
