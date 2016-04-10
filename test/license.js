8b8196fe6e6def622f014d213f38d7b4ab660033
Terin Stock <terinjokes@gmail.com>
2016-04-10 15:54:47 -0700

    chore(tests): remove hard coded year

--- test/license.js
+++ test/license.js
@@ -1,68 +1,69 @@
 'use strict';
 var test = require('tape'),
-	gulpLicense = require('../'),
-	Vinyl = require('vinyl');
+    gulpLicense = require('../'),
+    Vinyl = require('vinyl');
 
+var currentYear = (new Date()).getFullYear();
 var testContentsInput = 'module.exports = void 0';
-var testContentsExpected = '/*! (c) 2014 Terin Stock (MIT) */\nmodule.exports = void 0';
+var testContentsExpected = '/*! (c) ' + currentYear + ' Terin Stock (MIT) */\nmodule.exports = void 0';
 
 test('should add tiny license to file', function(t) {
-	t.plan(7);
+  t.plan(7);
 
-	var testFile = new Vinyl({
-		cwd: '/home/terin/broken-promises/',
-		base: '/home/terin/broken-promises/test',
-		path: '/home/terin/broken-promises/test/test1.js',
-		contents: new Buffer(testContentsInput)
-	});
+  var testFile = new Vinyl({
+    cwd: '/home/terin/broken-promises/',
+    base: '/home/terin/broken-promises/test',
+    path: '/home/terin/broken-promises/test/test1.js',
+    contents: new Buffer(testContentsInput)
+  });
 
-	var stream = gulpLicense('MIT', {tiny: true, organization: 'Terin Stock'});
+  var stream = gulpLicense('MIT', {tiny: true, organization: 'Terin Stock'});
 
-	stream.on('data', function(newFile) {
-		t.ok(newFile, 'emits a file');
-		t.ok(newFile.path, 'file has a path');
-		t.ok(newFile.relative, 'file has relative path information');
-		t.ok(newFile.contents, 'file has contents');
+  stream.on('data', function(newFile) {
+    t.ok(newFile, 'emits a file');
+    t.ok(newFile.path, 'file has a path');
+    t.ok(newFile.relative, 'file has relative path information');
+    t.ok(newFile.contents, 'file has contents');
 
-		t.ok(newFile instanceof Vinyl, 'file is Vinyl');
-		t.ok(newFile.contents instanceof Buffer, 'file contents are a buffer');
+    t.ok(newFile instanceof Vinyl, 'file is Vinyl');
+    t.ok(newFile.contents instanceof Buffer, 'file contents are a buffer');
 
-		t.equals(String(newFile.contents), testContentsExpected);
-	});
+    t.equals(String(newFile.contents), testContentsExpected);
+  });
 
-	stream.write(testFile);
-	stream.end();
+  stream.write(testFile);
+  stream.end();
 });
 
 test('should add license to file', function(t) {
-	t.plan(11);
+  t.plan(11);
 
-	var testFile = new Vinyl({
-		cwd: '/home/terin/broken-promises/',
-		base: '/home/terin/broken-promises/test',
-		path: '/home/terin/broken-promises/test/test1.js',
-		contents: new Buffer(testContentsInput)
-	});
+  var testFile = new Vinyl({
+    cwd: '/home/terin/broken-promises/',
+    base: '/home/terin/broken-promises/test',
+    path: '/home/terin/broken-promises/test/test1.js',
+    contents: new Buffer(testContentsInput)
+  });
 
-	var stream = gulpLicense('MIT', {organization: 'Terin Stock'});
+  var stream = gulpLicense('MIT', {organization: 'Terin Stock'});
 
-	stream.on('data', function(newFile) {
-		var contents = String(newFile.contents);
-		t.ok(newFile, 'emits a file');
-		t.ok(newFile.path, 'file has a path');
-		t.ok(newFile.relative, 'file has relative path information');
-		t.ok(newFile.contents, 'file has contents');
+  stream.on('data', function(newFile) {
+    var contents = String(newFile.contents);
+    t.ok(newFile, 'emits a file');
+    t.ok(newFile.path, 'file has a path');
+    t.ok(newFile.relative, 'file has relative path information');
+    t.ok(newFile.contents, 'file has contents');
 
-		t.ok(newFile instanceof Vinyl, 'file is Vinyl');
-		t.ok(newFile.contents instanceof Buffer, 'file contents are a buffer');
+    t.ok(newFile instanceof Vinyl, 'file is Vinyl');
+    t.ok(newFile.contents instanceof Buffer, 'file contents are a buffer');
 
-		t.true(/The above copyright notice/.test(contents), 'contains MIT license');
-		t.false(/{{ year }}/.test(contents), 'does not contain template');
-		t.true(/2014/.test(contents), 'does contain the current year');
-		t.false(/{{ organization }}/.test(contents), 'does not contain template');
-		t.true(/Terin Stock/.test(contents), 'does contain the author name');
-	});
+    t.true(/The above copyright notice/.test(contents), 'contains MIT license');
+    t.false(/{{ year }}/.test(contents), 'does not contain template');
+    t.true((new RegExp(currentYear)).test(contents), 'does contain the current year');
+    t.false(/{{ organization }}/.test(contents), 'does not contain template');
+    t.true(/Terin Stock/.test(contents), 'does contain the author name');
+  });
 
-	stream.write(testFile);
-	stream.end();
+  stream.write(testFile);
+  stream.end();
 });
--- test/streams.js
+++ test/streams.js
@@ -1,56 +1,57 @@
 'use strict';
 var test = require('tape'),
-	gulpLicense = require('../'),
-	Stream = require('stream'),
-	Readable = Stream.Readable,
-	Vinyl = require('vinyl'),
-	concat = require('concat-stream');
+    gulpLicense = require('../'),
+    Stream = require('stream'),
+    Readable = Stream.Readable,
+    Vinyl = require('vinyl'),
+    concat = require('concat-stream');
 
+var currentYear = (new Date()).getFullYear();
 var testContentsInput = 'module.exports = void 0';
-var testContentsExpected = '/*! (c) 2014 Terin Stock (MIT) */\nmodule.exports = void 0';
+var testContentsExpected = '/*! (c) ' + currentYear + ' Terin Stock (MIT) */\nmodule.exports = void 0';
 
 var testFile = new Vinyl({
-	cwd: '/home/terin/broken-promises/',
-	base: '/home/terin/broken-promises/test',
-	path: '/home/terin/broken-promises/test/test1.js',
-	contents: stringStream()
+  cwd: '/home/terin/broken-promises/',
+  base: '/home/terin/broken-promises/test',
+  path: '/home/terin/broken-promises/test/test1.js',
+  contents: stringStream()
 });
 
 test('should add license to streams', function(t) {
-	t.plan(7);
+  t.plan(7);
 
-	var stream = gulpLicense('MIT', {
-		tiny: true,
-		organization: 'Terin Stock'
-	});
+  var stream = gulpLicense('MIT', {
+    tiny: true,
+    organization: 'Terin Stock'
+  });
 
-	stream.on('data', function(newFile) {
-		t.ok(newFile, 'emits a file');
-		t.ok(newFile.path, 'file has a path');
-		t.ok(newFile.relative, 'file has relative path information');
-		t.ok(newFile.contents, 'file has contents');
+  stream.on('data', function(newFile) {
+    t.ok(newFile, 'emits a file');
+    t.ok(newFile.path, 'file has a path');
+    t.ok(newFile.relative, 'file has relative path information');
+    t.ok(newFile.contents, 'file has contents');
 
-		t.ok(newFile instanceof Vinyl, 'file is Vinyl');
-		t.ok(newFile.contents instanceof Stream, 'file contents are a Stream');
+    t.ok(newFile instanceof Vinyl, 'file is Vinyl');
+    t.ok(newFile.contents instanceof Stream, 'file contents are a Stream');
 
-		newFile.contents.pipe(concat({
-			encoding: 'string',
-		}, function(contents) {
-			t.equals(contents, testContentsExpected);
-		}));
-	});
+    newFile.contents.pipe(concat({
+      encoding: 'string'
+    }, function(contents) {
+      t.equals(contents, testContentsExpected);
+    }));
+  });
 
-	stream.write(testFile);
-	stream.end();
+  stream.write(testFile);
+  stream.end();
 });
 
 function stringStream() {
-	var stream = new Readable();
+  var stream = new Readable();
 
-	stream._read = function() {
-		this.push(testContentsInput);
-		this.push(null);
-	};
+  stream._read = function() {
+    this.push(testContentsInput);
+    this.push(null);
+  };
 
-	return stream;
+  return stream;
 }
